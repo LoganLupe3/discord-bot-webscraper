2e22deefbcd5d48295cd8900940fedd3e2ae42d0
LoganLupe3 <LoganLupeamanu@my.unt.edu>
2021-04-27 16:44:32 -0500

    Converted to discsord commands extension and added example commands

--- src/bot.py
+++ src/bot.py
@@ -8,23 +8,35 @@ import requests
 import json
 from bs4 import BeautifulSoup
 import discord
+from discord.ext import commands
+
+description = '''Discord CS:GO stat bot that fetches the user's CS:GO stats via webscraping.
+
+
+Various commands included.'''
+
+bot = commands.Bot(command_prefix='?', description=description)
 
 f = open("../constants/token.json")
 data = json.load(f)
 TOKEN = data["token"]
 
-client = discord.Client()
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------------')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers"""
+    await ctx.send(left + right)
 
-client.run(TOKEN)
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    await ctx.send(f'{member.name} joined in {member.joined_at}')
+
+
+bot.run(TOKEN)
